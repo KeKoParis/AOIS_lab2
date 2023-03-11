4080e201e6f15475c1a79d8e47d5681cb5a9c23d
Stanislav Voitkus <stanislav.voitkus@outlook.com>
2023-03-12 00:44:44 +0300

    guess finished

--- main.py
+++ main.py
@@ -97,56 +97,129 @@ def find_values_pdnf(curr_row):  # make pdnf form out of table
     return new_expression
 
 
-table = [[0 for i in range(4)] for j in range(8)]
+def conv_bin(expr):
+    bin_exp = ""
+    c = 0
+    for i in range(len(expr)):
+        if c == 1:
+            c = 0
+            continue
+        if expr[i] == "!":
+            c = 1
+            bin_exp += "1"
+        if expr[i] == "x":
+            bin_exp += "0"
+    return bin_exp
 
-check = 0
-for i in range(8):
-    if i < 4:
-        table[i][0] = 0
-    else:
-        table[i][0] = 1
 
-    if check == 4:
-        check = 0
+def conv_dec(curr_row):
+    dec = 0
+    for i in range(len(curr_row) - 1):
+        print(curr_row[i])
+        dec += int(curr_row[i]) * (2 ** (3 - i - 1))
+    print("dec ", str(dec))
+    return str(dec)
 
-    if check < 2:
-        table[i][1] = 0
-    else:
-        table[i][1] = 1
 
-    if i % 2 == 0:
-        table[i][2] = 0
-    else:
-        table[i][2] = 1
+def convert(curr_table):
+    coef = 7
+    index = 0
+    for i in curr_table:
+        index += i[3] * (2 ** coef)
+        coef = coef - 1
 
-    check = check + 1
+    return index
 
-expression = input("Enter expression: ")
 
-for i in range(8):
-    table = solve(table, expression, i)
+def fill_table(curr_table):
+    check = 0
+    for i in range(8):
+        if i < 4:
+            curr_table[i][0] = 0
+        else:
+            curr_table[i][0] = 1
 
-for i in table:
-    i[3] = int(i[3])
+        if check == 4:
+            check = 0
 
-for row in table:
-    print(row)
+        if check < 2:
+            curr_table[i][1] = 0
+        else:
+            curr_table[i][1] = 1
 
-pcnf: str = ""
-pdnf: str = ""
-for row in table:
-    if row[3] == 0:
-        pcnf += "("
-        pcnf += find_values_pcnf(row)
-        pcnf += ")*"
+        if i % 2 == 0:
+            curr_table[i][2] = 0
+        else:
+            curr_table[i][2] = 1
 
-    if row[3] == 1:
-        pdnf += "("
-        pdnf += find_values_pdnf(row)
-        pdnf += ")+"
+        check = check + 1
 
-pcnf = pcnf[:len(pcnf) - 1]
-pdnf = pdnf[:len(pdnf) - 1]
+    return curr_table
 
-print("\n pcnf", pcnf)
-print("\n pdnf", pdnf)
+
+def build_pcnf(curr_table):
+    pcnf: str = ""
+    bin_pcnf = ""
+    dec_pcnf = ""
+    for row in curr_table:
+        if row[3] == 0:
+            pcnf += "("
+            pcnf += find_values_pcnf(row)
+            pcnf += ")*"
+            bin_pcnf += conv_bin(find_values_pcnf(row))
+            bin_pcnf += " "
+            dec_pcnf += conv_dec(row) + " "
+
+    return pcnf, dec_pcnf, bin_pcnf
+
+
+def build_pdnf(curr_table):
+    pdnf: str = ""
+    bin_pdnf = ""
+    dec_pdnf = ""
+    curr_pdnf = ""
+    for row in curr_table:
+        if row[3] == 1:
+            pdnf += "("
+            pdnf += find_values_pdnf(row)
+            pdnf += ")+"
+            bin_pdnf += conv_bin(find_values_pdnf(row))
+            bin_pdnf += " "
+            curr_pdnf = conv_bin(find_values_pdnf(row))
+            dec_pdnf += conv_dec(curr_pdnf) + " "
+
+    return pdnf, bin_pdnf, dec_pdnf
+
+
+def main():
+    table = [[0 for i in range(4)] for j in range(8)]
+
+    table = fill_table(table)
+
+    expression = input("Enter expression: ")
+
+    for i in range(8):
+        table = solve(table, expression, i)
+
+    for i in table:
+        i[3] = int(i[3])
+
+    for row in table:
+        print(row)
+
+    pcnf, bin_pcnf, dec_pcnf = build_pcnf(table)
+    pdnf, bin_pdnf, dec_pdnf = build_pdnf(table)
+
+    pcnf = pcnf[:len(pcnf) - 1]
+    pdnf = pdnf[:len(pdnf) - 1]
+
+    print("\n pcnf", pcnf)
+    print("\n bin ", bin_pcnf)
+    print("\n dec ", dec_pcnf)
+    print("\n pdnf", pdnf)
+    print("\n bin ", bin_pdnf)
+    print("\n dec ", dec_pdnf)
+    print("\n index ", convert(table))
+
+
+main()
